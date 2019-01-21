8331a8b7fbd44820ecaf0e33e93270dc080e505f
muslimbeibytuly <muslimbeibytuly@gmail.com>
2019-01-21 21:02:02 +0600

    Add .env and refactor settings.py

--- src/core/settings.py
+++ src/core/settings.py
@@ -11,10 +11,15 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from types import MappingProxyType
 
 import environ
 
-env = environ.Env()
+env = environ.Env(
+    DEBUG=(bool, True),
+    SECRET_KEY=(str, 'ee9a4958-1881-4e2f-97bc-e347562ddb58'),
+    DATABASE_URL=(str, 'psql://onekbtuadmin:UFR9Uy7EQH54p2P3bZ76@db:5432/onekbtu')
+)
 
 environ.Env.read_env()
 
@@ -28,21 +33,21 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ('*',)
 
 # Application definition
 
 INSTALLED_APPS = (
-    'rest_framework',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
 
     'blog'
 )
@@ -59,21 +64,25 @@ MIDDLEWARE = (
 
 ROOT_URLCONF = 'core.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+TEMPLATES = (
+    MappingProxyType(
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': (),
+            'APP_DIRS': True,
+            'OPTIONS': MappingProxyType(
+                {
+                    'context_processors': (
+                        'django.template.context_processors.debug',
+                        'django.template.context_processors.request',
+                        'django.contrib.auth.context_processors.auth',
+                        'django.contrib.messages.context_processors.messages',
+                    ),
+                }
+            ),
+        }
+    ),
+)
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
@@ -87,20 +96,28 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+AUTH_PASSWORD_VALIDATORS = (
+    MappingProxyType(
+        {
+            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        }
+    ),
+    MappingProxyType(
+        {
+            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        }
+    ),
+    MappingProxyType(
+        {
+            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        }
+    ),
+    MappingProxyType(
+        {
+            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        }
+    ),
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
