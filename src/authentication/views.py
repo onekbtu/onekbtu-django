e819343293d135e004d6447713a753209c776ae5
muslimbeibytuly <muslimbeibytuly@gmail.com>
2019-02-08 00:22:06 +0600

    Fix migrations

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics, permissions, status
-from rest_framework.response import Response
+from rest_framework import generics, permissions
 
 from authentication.serializers import UserSerializer
 
@@ -9,10 +8,3 @@ class RegisterAPIView(generics.CreateAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
     permission_classes = (permissions.AllowAny,)
-
-    def create(self, request, *args, **kwargs) -> Response:
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
