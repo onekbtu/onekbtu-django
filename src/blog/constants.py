df64bc8acc9280ebe0b89cdb631446f692b8e1b0
alan <alan@relog.kz>
2019-02-07 23:21:42 +0600

    [] => ()

--- src/blog/constants.py
+++ src/blog/constants.py
@@ -1,5 +1,5 @@
 # Tags suitable for rendering markdown
-MARKDOWN_TAGS = [
+MARKDOWN_TAGS = (
     "h1", "h2", "h3", "h4", "h5", "h6",
     "b", "i", "strong", "em", "tt",
     "p", "br",
@@ -7,9 +7,9 @@ MARKDOWN_TAGS = [
     "ul", "ol", "li", "dd", "dt",
     "img",
     "a",
-]
+)
 
 MARKDOWN_ATTRS = {
-    "img": ["src", "alt", "title"],
-    "a": ["href", "alt", "title"],
+    "img": ("src", "alt", "title"),
+    "a": ("href", "alt", "title"),
 }
